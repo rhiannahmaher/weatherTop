c2ff4d4a4181716c2276c3f0834ac3933cae6963
Glitch (befitting-working-enquiry) <none>
2024-08-12 17:29:24 +0000

    ⏰🏑 Checkpoint ./views/dashboard-view.hbs:68602827/207 ./views/partials/list-playlist.hbs:68602827/119 ./views/partials/list-playlists.hbs:68602827/121 ./views/partials/add-playlist.hbs:68602827/280 ./routes.js:68602827/117 ./controllers/dashboard-controller.js:68602827/2004 ./models/playlist-store.js:68602827/840

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,9 +1,21 @@
+import { playlistStore } from "../models/playlist-store.js";
+
 export const dashboardController = {
   async index(request, response) {
     const viewData = {
-      title: 'Playlist 1 Dashboard'
+      title: "Playlist Dashboard",
+      playlists: await playlistStore.getAllPlaylists(), // lists playlists
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
+  },
+  
+  async addPlaylist(request, response) {
+    const newPlaylist = {
+      title: request.body.title,
+    };
+    console.log(`adding playlist ${newPlaylist.title}`);
+    await playlistStore.addPlaylist(newPlaylist); // statement to add playlist
+    response.redirect("/dashboard");
   },
 };
