0e95c58c2b4074687feb72f816d69b7353793d44
Glitch (befitting-working-enquiry) <none>
2024-08-12 19:12:30 +0000

    ⛷🐌 Checkpoint ./controllers/station-controller.js:68602827/512 ./views/station-view.hbs:68602827/117 ./routes.js:68602827/589 ./views/partials/list-stations.hbs:68602827/317

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
+import { stationController } from "./controllers/station-controller.js";
 
 export const router = express.Router();
 
@@ -8,4 +9,4 @@ router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 router.post("/dashboard/addstation", dashboardController.addStation); // allows button to work for add playlist
-
+router.get("/station/:id", stationController.index);
