4486757be3bbe372aa092874a7a97f458acaae6e
Glitch (befitting-working-enquiry) <none>
2024-08-13 11:36:14 +0000

    👌🐋 Checkpoint ./models/user-store.js:68602827/850 ./views/index.hbs:68602827/89 ./views/partials/welcome-menu.hbs:68602827/543 ./views/login-view.hbs:68602827/569 ./views/signup-view.hbs:68602827/966 ./controllers/accounts-controller.js:68602827/1302 ./routes.js:68602827/809 ./views/partials/menu.hbs:68602827/1066

--- routes.js
+++ routes.js
@@ -2,13 +2,23 @@ import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
+import { accountsController } from './controllers/accounts-controller.js';
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
+router.post("/dashboard/addstation", dashboardController.addStation);  // allows button to work for add playlist
+
 router.get("/about", aboutController.index);
-router.post("/dashboard/addstation", dashboardController.addStation); // allows button to work for add playlist
+
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/addreport", stationController.addReport);
+
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 
